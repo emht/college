9cfc6e28c62322c6a5f3d434675b41fc35f24db6
emht <emht@users.noreply.github.com>
2017-08-29 00:36:47 +0530

    Updating bash for best input

--- ADA_Lab/week1/input.sh
+++ ADA_Lab/week1/input.sh
@@ -1,17 +1,43 @@
-#! /usr/bin/bash
-set -x
-set -e
+#! /bin/bash
+# set -x
+# set -e
 
-
-# This is the script to genrate the input for Best, Average and Worst Case Scenarious
 # Usage: ./input.sh -[best/avg/worst] [size]
 # It will store the generated inputs in best.txt, average.txt and worst.txt
+# This is the script to genrate the input for Best, Average and Worst Case Scenarious
 
+# Defining the size of the input you need to generated, default value is 10
+if [ "$#" -ne 2 ]; then
+	SIZE=10
+else
+	SIZE=$2
+fi
+
+# Clearing the files to remove redundancy
+ > best.txt
+ > average.txt
+ > worst.txt
+
+#Checking for the type of input required
+
+best=false
+avg=false
+worst=false
+
+if [ $1 = "-best" ]; then
+	best=true
+elif [ $1 = "-avg" ]; then
+	avg=true
+elif [ $1 = "-worst" ]; then 
+	worst=true
+else
+	echo "Please provide proper arguments."
+fi
 
 #Best case Generation
-for num in {0..$SIZE}
+fucntion best {
+	for num in `seq 1 $SIZE`;
 	do
-
+		echo $num >> best.txt
 	done
-
-
+}

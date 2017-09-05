1ae1909ceecf033c4c14c8f0e63ad71643888d35
emht <emht@users.noreply.github.com>
2017-09-05 20:34:04 +0530

    Week2 Files

---
+++ ADA_Lab/week1/mergesort.py
@@ -0,0 +1,30 @@
+'''
+Takes the input from a file containing 'N' numbers
+which outputs the 'N' and time to sort it via InsertionSort.
+'''
+# imports for calculating the time take to execute the script
+import time
+
+def InsertionSort(Arr):    
+    length = len(Arr)
+    StartTime = time.time()
+    for i in range(1, length):
+        tmp = Arr[i]
+        k = i
+        while k > 0 and tmp < Arr[k - 1]:
+            Arr[k] = Arr[k - 1]
+            k -= 1
+        Arr[k] = tmp
+    
+    TimeTaken = time.time() - StartTime
+    # outputs the time taken in seconds
+    print(length, float(round(TimeTaken, 5)))
+
+
+
+
+if __name__ == "__main__" :
+    Arr = input().split(' ')
+    Arr.remove('')
+    Arr = list(map(int, Arr))
+    InsertionSort(Arr)

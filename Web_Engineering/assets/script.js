7a27b8ece7841429830c640810fcbeae5c1360fd
emht <emht@users.noreply.github.com>
2018-04-11 23:04:14 +0530

    Adding client side form validation check.

--- Web_Engineering/assets/script.js
+++ Web_Engineering/assets/script.js
@@ -6,3 +6,18 @@ function cal_interest() {
     var final_interest = parseFloat((principal * rate * months) / 100);
     document.getElementById('simple_interest').innerHTML="Total amount after interest is: " + final_interest;
 }
+
+function validate_form() {
+    if (document.forms["contact_form"]["firstname"].value == "" ) {
+        alert("Please provide your Name!");
+        return false;
+    }
+    if (document.forms["contact_form"]["email"].value == "") {
+        alert("Please provide your email address to procceed!");
+        return false;
+    }
+    if (document.forms["contact_form"]["message"].value == "") {
+        alert("You cannot leave the message empty!");
+        return false;
+    }
+}
